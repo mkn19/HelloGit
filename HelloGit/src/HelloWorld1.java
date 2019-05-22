1374ab090c83eecfb447787807221e52feacab40
marcu <marcu@139.174.111.119>
2019-05-22 09:58:52 +0200

    initial

---
+++ HelloGit/src/HelloWorld1.java
@@ -0,0 +1,17 @@
+/**
+ * Class to ork with Git
+ * @author marcu
+ *
+ */
+public class HelloWorld1 {
+
+	/**
+	 * Method just prints Hello World to the terminal.
+	 * @param args No arguments needed.
+	 */
+	public static void main (String[] args)	{
+		System.out.println("Hello World 1!");
+		System.out.println("Hello World 2!");
+	}
+	
+}
