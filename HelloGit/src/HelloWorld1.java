7dea4310615ee85814307334b3cf6c887069aeff
mkn19 <50904176+mkn19@users.noreply.github.com>
2019-05-22 10:03:36 +0200

    Update HelloWorld1.java

--- HelloGit/src/HelloWorld1.java
+++ HelloGit/src/HelloWorld1.java
@@ -12,6 +12,8 @@ public class HelloWorld1 {
 	public static void main (String[] args)	{
 		System.out.println("Hello World 1!");
 		System.out.println("Hello World 2!");
+		System.out.println("Ich habe fertig!");
+		
 	}
 	
 }
